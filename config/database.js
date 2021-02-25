9e3c5ac06229085ee3c347f65e39c48b943cc6e3
Patrick Castro <patrickp.castro@gmail.com>
2021-02-25 14:20:44 +0800

    remove config.js from gitignore

---
+++ config/database.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    username: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASS,
+    database: 'clean_node_ddd_dev',
+    host: process.env.DB_LOCAL_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_TEST_USER,
+    password: process.env.DB_TEST_PASS,
+    database: 'clean_node_ddd_test',
+    host: process.env.DB_TEST_HOST,
+    dialect: 'mysql',
+    logging: false
+  },
+  production: process.env.DATABASE_URL
+}
