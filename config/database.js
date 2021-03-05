f6300a17e96f5e5bb09b5299b2f2f794a50183a8
Patrick Castro <patrickp.castro@gmail.com>
2021-03-05 18:30:52 +0800

    database.js

--- config/database.js
+++ config/database.js
@@ -2,17 +2,17 @@ module.exports = {
   development: {
     username: process.env.DB_LOCAL_USER,
     password: process.env.DB_LOCAL_PASS,
-    database: 'clean_node_ddd_dev',
+    database: 'clean_node_ddd',
     host: process.env.DB_LOCAL_HOST,
     dialect: 'mysql'
   },
   test: {
     username: process.env.DB_TEST_USER,
     password: process.env.DB_TEST_PASS,
-    database: 'clean_node_ddd_test',
+    database: 'clean_node_ddd',
     host: process.env.DB_TEST_HOST,
     dialect: 'mysql',
     logging: false
   },
   production: process.env.CLEARDB_DATABASE_URL
-}
+};
